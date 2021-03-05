e4d2eb3a1c85d7ab53196656e93c3a4cea5112e4
ItamarHavenstein <itamarhavenstein@hotmail.com>
2021-03-04 22:46:51 -0300

    alterado classe de funcionarios para ter metodos de calculo de salario liquido e aumento

--- Primeiro/Models/Funcionario.cs
+++ Primeiro/Models/Funcionario.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Globalization;
 
 namespace Primeiro.Models
 {
   class Funcionario
   {
     public string Nome { get; set; }
-    public double Salario { get; set; }
+    public double SalarioBruto { get; set; }
+    public double Imposto { get; set; }
+
+    public double SalarioLiquido(){
+        return SalarioBruto - Imposto;
+    }
+
+    public void AumentarSalario(double porcentagem){
+      SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);  
+    }
+
+    public override string ToString()
+    {
+      return Nome +", $"+ SalarioLiquido().ToString("f2", CultureInfo.InvariantCulture);
+    }
   }
 }
