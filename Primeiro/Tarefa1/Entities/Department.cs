a89a5217100e261f0e96901c9ea518a100f055d6
ItamarHavenstein <itamarhavenstein@hotmail.com>
2021-08-18 22:01:22 -0300

    Tarefa 1 entities e enums para fazer o contrato de trabalho

---
+++ Primeiro/Tarefa1/Entities/Department.cs
@@ -0,0 +1,14 @@
+namespace Primeiro.Tarefa1.Entities
+{
+  public class Department
+  {
+    public string Name { get; set; }
+
+    public Department() { }
+
+    public Department(string name)
+    {
+      Name = name;
+    }
+  }
+}
---
+++ Primeiro/Tarefa1/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+namespace Primeiro.Tarefa1.Entities.Enums
+{
+  public enum WorkerLevel : int
+  {
+    Junior = 0,
+    MidLevel = 1,
+    Senior = 2
+  }
+}
