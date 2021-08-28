499609ab4f3d52593af92d45d190f6fec1771d24
ItamarHavenstein <itamarhavenstein@hotmail.com>
2021-08-27 22:03:36 -0300

    aprendendo um pouco sobre herança

---
+++ Primeiro/Services/Heranca/AulaHeranca.cs
@@ -0,0 +1,10 @@
+namespace Primeiro.Services.Heranca
+{
+  public class AulaHeranca
+  {
+    public void HerancaAccount()
+    {
+        //utilizando a classe Account e BusinessAccount para representar com funciona Herança no sistema
+    }
+  }
+}
